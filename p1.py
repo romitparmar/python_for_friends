00b87bc22a81773bc492a100f4552cebcf77af1b
romitparmar <159528471+romitparmar@users.noreply.github.com>
2024-08-04 22:26:53 +0530

    practical1

---
+++ p1.py
@@ -0,0 +1,113 @@
+#--------------practicle no.1-------------#
+
+# name =input("enter name: ")
+# contact = int(input("enter contact number: "))
+# email = input("enter e-mainl: ")
+# birthdate = int(input("enter birthdate: "))
+# print(f"the name of person is:{name}")
+# print(f"the contact of the person  is:{contact}")
+# print(f"the email of the person  is:{email}")
+# print(f"the birthdate of person is:{birthdate}")
+
+
+     #-----practicle no.2(a)--------#
+# c = float(input("enter celsius: "))
+# f = (9/5)*c+32
+# print(f"the fahrenheit: {f}c")
+
+# --------------practicle no.2(b)--------------------#
+# x1 = int(input("enter x1: "))
+# x2 = int(input("enter x2: "))
+# y1 = int(input("enter y1: "))
+# y2 = int(input("enter y2: "))
+
+# slop = (y2-y1)/(x2-x1)
+# print(f"the slop of a line between two points: {slop}")
+
+# --------------practicle no.2(c)--------------------#
+# p = float(input("enter p: "))
+# r = float(input("enter r: "))
+# t = int(input("enter time: "))
+# n = 1
+# simple_intrest = (p*r*t)/100
+# print(f"simple intrest is: {simple_intrest}")
+# compound_intrest = p*(1+(r/100*n))**(n*t)
+# print(f"the compound intrenst is:{compound_intrest}")
+
+
+# --------------practicle no.2(d)--------------------#
+
+# a = int(input("enter the value of a: "))
+# b = int(input("enter the value of b: "))
+# c = int(input("enter the value of c: "))
+
+# if a>b and a>c:
+#     print("a is max")
+# elif b>a and b>c:
+#     print("b is max")
+# else:
+#     print("c is max")
+
+# --------------practicle no.2(e)--------------------#
+
+# pi = 3.14
+# r = int(input("enter the r: "))
+# print((4*pi*r**2))
+# print(((4/3)*pi)*r**3)
+
+# --------------practicle no.2(f)--------------------#
+
+
+# a = int(input("enter the value of a: "))
+# b = int(input("enter the value of b: "))
+# c = int(input("enter the value of c: "))
+
+# discriminant = b**2 - 4*a*c
+# print("Discriminant:", discriminant)
+
+# if discriminant == 0:
+#     real_root = -b / (2*a)
+#     print("Single real root:", real_root)
+# elif discriminant > 0:
+#     root1 = (-b + discriminant**0.5) / (2*a)
+#     root2 = (-b - discriminant**0.5) / (2*a)
+#     print("Two real roots:")
+#     print("Root 1:", root1)
+#     print("Root 2:", root2)
+# else:
+#     print("No real roots (complex roots exist)")
+
+
+
+
+# a = 10
+# b = 20
+# c = 30
+
+# print("ax^2 + bx + c = 0")
+
+# disc = (b*b)-(4*a*c)
+
+# if disc<0:
+#     root1 = (-b + disc**(1/2))/(2*a)
+#     root2 = (-b - disc**(1/2))/(2*a)
+#     print(root1, root2)
+#     print(type(root1))
+# else:
+#     root1 = (-b + disc**(1/2))/(2*a)
+#     root2 = (-b - disc**(1/2))/(2*a)
+#     print(f"real roots are:  {root1} and {root2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
